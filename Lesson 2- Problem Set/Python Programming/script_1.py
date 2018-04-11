9f8b005a585eca223c02fb9d8179382145927650
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-04-11 14:09:13 -0500

    Create script_1.py

---
+++ Lesson 2- Problem Set/Python Programming/script_1.py
@@ -0,0 +1,8 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+
+
+hours_in_day = 24
+days_in_week = 7
+weeks_in_course = 7
+total_hours = (hours_in_day * days_in_week * weeks_in_course)
+print total_hours
