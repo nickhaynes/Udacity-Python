98856dc224e248563e039ae851a9d4427aec2284
Nick Haynes <36779922+nickhaynes@users.noreply.github.com>
2018-04-11 20:57:32 -0500

    Create script.py

---
+++ Lesson 6- Problem Set/6-3 Median/script.py
@@ -0,0 +1,37 @@
+# Define a procedure, median, that takes three
+# numbers as its inputs, and returns the median
+# of the three numbers.
+
+# Make sure your procedure has a return statement.
+
+def bigger(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+def biggest(a,b,c):
+    return bigger(a,bigger(b,c))
+
+def median(a,b,c):
+    if b>=a>=c:
+        return a
+    if c>=a>=b:
+        return a
+    if c>=b>=a:
+        return b
+    if a>=b>=c:
+        return b
+    else:
+        return c
+
+
+print(median(1,2,3))
+#>>> 2
+
+print(median(9,3,6))
+#>>> 6
+
+print(median(7,8,7))
+#>>> 7
+
